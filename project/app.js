760969c474e824f94e975552cb994746d4b2919e
adambourhim <benbourhim7@gmail.com>
2021-10-11 10:52:32 +0300

    operation_system

--- project/app.js
+++ project/app.js
@@ -1,7 +1,10 @@
 const calculator = require("./calc");
 
 const path = require("path");
+const myOs = require("os");
 
 //console.log(calculator.sub(60, 40));
 
-console.log(path.parse(__filename));
+//console.log(path.parse(__filename));
+
+console.log(myOs.cpus());
